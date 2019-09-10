2bf4ead44a9ebe31c437948a91b807058615aae6
Taylor Covington <tcovingtondev@gmail.com>
2019-09-10 16:16:37 -0600

    started product controller

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,7 +2,7 @@ class ProductsController < ApplicationController
   before_action :set_store
   before_action :set_product, only: [:show, :edit, :update, :destroy]
   def index
-    @products = @store.products.all
+    @products = @store.products
   end
 
   def show
@@ -12,15 +12,40 @@ class ProductsController < ApplicationController
   def new
   end
 
+  def create
+    @product = Product.new(product_params)
+    if @product.save
+      redirect_to store_path(@store)
+    else
+      render :new
+    end
+  end
+
   def edit
+  end
+
+  def update
+    if @product.update(product_params)
+      redirect_to store_path(store.id)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @product.destroy
+    redirect_to store_path(@store)
   end
 
   private
   def set_store
-    @store = Store.find(params[:id])
+    @store = Store.find(params[:store_id])
   end
   def set_product
-    @product = @store.product.find(params[:id])
+    @product = Product.find(params[:id])
+  end
+  def product_params
+    params.require(:product).permit(:product_name, :product_price)
   end
 
 end
