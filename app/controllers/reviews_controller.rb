6be39b8349839e56c86f13f79d595a9572168922
Taylor Covington <tcovingtondev@gmail.com>
2019-09-11 22:36:45 -0600

    added styling

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,14 +1,25 @@
 class ReviewsController < ApplicationController
+  before_action :set_product
+  before_action :set_review, only: [:show, :edit, :update, :destroy]
   def index
     @reviews = @product.reviews.all
   end
 
   def show
+
   end
 
   def new
   end
 
   def edit
+  end
+
+  private
+  def set_review
+    @review = Review.find(params[:id])
+  end
+  def set_product
+    @product = Product.find(params[:product_id])
   end
 end
