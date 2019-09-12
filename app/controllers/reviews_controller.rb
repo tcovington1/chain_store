ea35a0a63f35062e9da2c0b0966a8d64a3219c4a
Taylor Covington <tcovingtondev@gmail.com>
2019-09-12 14:32:44 -0600

    review page

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -10,16 +10,45 @@ class ReviewsController < ApplicationController
   end
 
   def new
+    @review = Review.new
   end
 
+  def create
+    @review = @product.reviews.new(review_params)
+    if @review.save
+      redirect_to product_reviews_path(@product.id)
+    else
+      render :new
+    end
+  end
   def edit
   end
+
+  def update
+    @review = Review.find(params[:id])
+      
+      if @review.update(review_params)
+        redirect_to product_reviews_path(@product.id)
+      else
+        render :edit
+      end
+    end
+
+    def destroy
+      @review.destroy
+      redirect_to product_reviews_path(@product.id)
+    end
 
   private
   def set_review
     @review = Review.find(params[:id])
   end
+
   def set_product
     @product = Product.find(params[:product_id])
+  end
+
+  def review_params
+    params.require(:review).permit(:name, :subject, :message)
   end
 end
